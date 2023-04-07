5b5569e3f92da104de3bf152648db5ec4d56cf07
Arman Sargsyan <lillimb@mail.ru>
2023-04-07 17:23:44 +0400

    test
    test

---
+++ language/ru-ru/header.php
@@ -0,0 +1,19 @@
+<?php
+// Text
+$_['text_company']                 =     'Компания "АКОМ – Автоматизация и КОМмуникации';
+$_['text_city']                    =     'Челябинск';
+$_['text_sales_department']        =     'Отдел продаж:';
+$_['text_mail_sales_department']   =     'sales@i-nets.ru';
+$_['text_department_support']      =     'Отдел тех.поддержки:';
+$_['text_mail_department_support'] =     'support@i-nets.ru';
+$_['text_video_tutorials']         =     'Видеоуроки';
+$_['text_about_us']                =     'О компании';
+$_['text_support']                 =     'Поддержка';
+$_['text_faq']                     =     'FAQ';
+$_['text_reviews']                 =     'Отзывы';
+$_['text_news']                    =     'Новости';
+$_['text_contacts']                =     'Контакты';
+$_['text_number']                  =     '8 800 222 32 40';
+$_['text_request_call']            =     'Заказать звонок';
+
+
